bffde8fb9740bed47d2efd5304790f1f265480fa
sathwikchiliveri <chiliverisathwik@gmail.com>
2025-12-23 13:37:02 +0000

    chore: clean frontend boilerplate and add base structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>School Portal – Frontend Ready</h1>
+    </div>
+  );
+}
+
+export default App;
