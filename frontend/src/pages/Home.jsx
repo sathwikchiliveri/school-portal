9cc1f59931382d90521cc8ce148cd5dd94bb9c50
sathwikchiliveri <chiliverisathwik@gmail.com>
2026-01-16 19:29:16 +0000

    UPDATES

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="text-3xl font-bold text-gray-800">
       Home Page
+      <div className="bg-red-500 text-white p-4">TAILWIND OK</div>
     </div>
   );
 }
