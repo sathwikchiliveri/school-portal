9c6b568c4e42c681beecbc31c7e73f2e14aac9c0
sathwikchiliveri <chiliverisathwik@gmail.com>
2026-01-17 17:35:30 +0000

    UPDATE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div className="text-3xl font-bold text-gray-800">
       Home Page
+      <div className="text-blue-600 text-4xl font-bold">
+        TAILWIND IS WORKING
+      </div>
       <div className="bg-red-500 text-white p-4">TAILWIND OK</div>
     </div>
   );
